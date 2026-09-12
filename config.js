de24157058fc49df973999e54af3bb413542ca64
liakh10 <119670997+liakh10@users.noreply.github.com>
2026-09-13 02:52:52 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* ================= CONFIG: единственное место, где меняются данные ================= */
 window.SITE = {
-  CA:      "SOON",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
+  CA:      "0x2b0630d280Df16FC08D10ad00C9740C017FaC516",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
   X_URL:   "https://x.com/flyholexapp_rh",
   NAME:    "Flyholex",
   TICKER:  "$FLYHOLEX",
